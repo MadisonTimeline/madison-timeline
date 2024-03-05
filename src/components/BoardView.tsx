305a6795f07718ae53e785c742389818426183a8
Freddy <wdragj@gmail.com>
2024-03-05 14:57:33 -0600

    Fetch from DB implementation.

--- src/components/BoardView.tsx
+++ src/components/BoardView.tsx
@@ -1,17 +1,35 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CommentSection from "./CommentSection";
 import { Post } from "@/types/Post";
 import CreatePost from "./CreatePost";
 import { Button } from "./ui/button";
 import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 import PostPreview from "./PostPreview";
+import { createClient } from "@/utils/supabase/client";
+
+const supabase = createClient();
+
+async function fetchPosts(boardname: string): Promise<Post[]> {
+    const { data, error } = await supabase.from("posts").select().eq("board_name", boardname);
+
+    if (error) {
+        console.log("Error fetching posts:");
+        console.error(error);
+    }
+    return data as Post[];
+}
 
 function BoardView({ boardname }: { boardname: string }) {
     const [posts, setPosts] = useState<Post[]>([]);
 
     const [createPostModal, setCreatePostModal] = useState(false);
     const { isLoading, user } = useKindeBrowserClient();
+
+    // Use effect to fetch posts on mount
+    useEffect(() => {
+        fetchPosts(boardname).then(setPosts);
+    }, [boardname]);
 
     if (isLoading) return <div>Loading...</div>;
     return (
