4af1c3057d6b11bf89a137c7a12482247bb5f734
DK Kim <dkim693@wisc.edu>
2024-03-09 01:36:26 -0600

    add useeffect

--- src/components/PostPreview.tsx
+++ src/components/PostPreview.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { Button } from "@/components/ui/button"
 import {
     Card,
