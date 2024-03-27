407644cc6329f8f015742e8ab055bc156992d5ba
Jin <jinkimcs@gmail.com>
2024-03-26 22:27:37 -0500

    get boards API

---
+++ src/app/api/getBoard/[name]/route.ts
@@ -0,0 +1,45 @@
+import { createClient } from "@/utils/supabase/client";
+export const dynamic = "force-dynamic"; // Use this if you need to ensure the function is always executed dynamically
+
+export async function GET(request: Request) {
+    try {
+        const receivedBoardName = request.url.split('/').pop(); // Extracting board name from URL
+        const supabase = createClient();
+        const { data, error } = await supabase
+            .from("boards")
+            .select("*")
+            .eq("name", receivedBoardName);
+
+        if (error) {
+            throw new Error(error.message);
+        }
+
+        if (!data || data.length === 0) {
+            return new Response(
+                JSON.stringify({ message: "Board not found" }),
+                {
+                    headers: { "Content-Type": "application/json" },
+                    status: 404
+                }
+            );
+        } else {
+            return new Response(
+                JSON.stringify(data[0]),
+                {
+                    headers: { "Content-Type": "application/json" },
+                    status: 200
+                }
+            );
+        }
+
+    } catch (error) {
+        console.error("Error processing request:", error);
+        return new Response(
+            JSON.stringify({ message: "Error processing request" }),
+            {
+                headers: { "Content-Type": "application/json" },
+                status: 500
+            }
+        );
+    }
+}
