c5cb073654c38c5b73463c9a733ae19eaa3a2070
DK Kim <dkim693@wisc.edu>
2024-04-08 04:35:25 -0500

    hot fix added project id to cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  projectId: "wwor6x",
   e2e: {
     baseUrl: 'http://localhost:3000',
+    
     setupNodeEvents(on, config) {
         // implement node event listeners here
       },
