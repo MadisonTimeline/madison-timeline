e85838900f95cd2e66ae9b15d8919622d2f424fa
HanYeyoung <hany2813@naver.com>
2024-04-09 02:40:02 -0500

    add design page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,25 @@
 import Image from "next/image";
-import Navbar from "@/components/NavBar";
 import Link from "next/link";
+import logo from '../logo.png';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen min-w-screen flex-col items-center justify-between p-24">
-      <h1 className="text-6xl font-bold mt-[100px]">Welcome to Madison Timeline</h1>
-      <Link href="/boards" className="text-3xl text-white font-bold border rounded p-2 bg-[#C5050C] shadow"> Get Started </Link>
+    <main className="flex h-screen w-screen flex-col items-center justify-center bg-white text-center">
+      <Image src={logo} width={300} height={300} alt="Madison Timeline Logo" priority />
+      
+      <h2 className="mt-12 mb-16 text-4xl font-bold" style={{ fontFamily: '"Sansita Swashed", cursive' }}>
+        Welcome to Madison Timeline
+      </h2>
+      
+      <div className="mb-16 flex flex-col items-center space-y-1">
+        <Link href="/login" passHref className="flex justify-center items-center w-80 h-14 bg-[#D80100] text-white font-bold rounded-3xl">
+            LOGIN
+        </Link>
+
+        <Link href="/boards" passHref className="flex justify-center items-center w-80 h-14 bg-[#D9D9D9] text-black font-bold rounded-3xl">
+            START AS GUEST
+        </Link>
+      </div>
     </main>
   );
 }
