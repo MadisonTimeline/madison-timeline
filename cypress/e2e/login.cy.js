9e7cdb6395a03d3ca559f65800c87915bf395b2f
DK Kim <dkim693@wisc.edu>
2024-04-09 00:20:07 -0500

    add login test

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,28 @@
+// test that logs in a user
+describe('Login', () => {
+    it('should login a user', () => {
+        cy.visit('https://madison-timeline.vercel.app/boards');
+        // Button should contain 'Sign in'
+        cy.contains('Sign in').click();
+
+        // switch origin to login page
+
+        cy.origin('https://madta.kinde.com', () => {
+            cy.url().should('include', 'https://madta.kinde.com/auth');
+            // Input email
+            cy.contains('Email').click().type('dkim693@wisc.edu');
+            cy.contains('Continue').click();
+            // Input password
+            cy.contains('Password').click().type('password1234');
+            cy.contains('Continue').click();
+            cy.wait(5000);
+        })
+        cy.wait(5000);
+        cy.url().should('include', '/boards');
+                // button should contain 'Log out;
+                cy.contains('Log out').click();
+                // should be redirected to landing page
+                cy.url().should('include', 'https://madison-timeline.vercel.app/');
+    });
+}
+);
