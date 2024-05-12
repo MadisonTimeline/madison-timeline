0b89cb8d71ab0acfa17af239f21b70f7b780de2c
DK Kim <dkim693@wisc.edu>
2024-05-13 00:47:22 +0900

    added fix layout plan

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,6 +3,9 @@ import Head from 'next/head'
 import Sidebar from './Sidebar';
 import MenuBarMobile from './MenuBarMobile';
 
+
+// should fix layout so that it is separate from its children
+// should separate all useState and useEffect hooks into their own components
 export default function Layout({ pageTitle, children }: { pageTitle: string, children: React.ReactNode }) {
 
     // Mobile sidebar visibility state
