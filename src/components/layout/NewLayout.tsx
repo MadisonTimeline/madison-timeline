cdee43a6fc3eb56cee734770667d9c979cfa08ba
DK Kim <145156139+DK-Kim4312@users.noreply.github.com>
2024-06-06 23:09:57 +0900

    fixed sidebar

--- src/components/layout/NewLayout.tsx
+++ src/components/layout/NewLayout.tsx
@@ -1,17 +1,16 @@
-"use client";
-import React, { useState, useEffect } from "react";
-import MenuBarMobile from "./MenuBarMobile";
-import Sidebar from "./Sidebar";
+
+// import MenuBarMobile from "./MenuBarMobile";
+// import Sidebar from "./Sidebar";
+import { Sidebar } from "./sidebar/sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-    const [showSidebar, setShowSidebar] = useState(false);
+    // const [showSidebar, setShowSidebar] = useState(false);
 
     return (
         <div className="min-h-screen min-w-screen max-w-screen">
             <div className="flex">
-                <MenuBarMobile setter={setShowSidebar} />
-                <Sidebar show={showSidebar} setter={setShowSidebar} />
-                <div className="flex flex-col flex-grow justify-center align-center bg-background w-screen md:w-full min-h-screen">
+                <Sidebar />
+                <div className="pl-[56px] flex flex-col flex-grow justify-center align-center bg-background">
                     {children}
                 </div>
             </div>
